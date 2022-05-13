e95eb3fa506c25fcf940c9d2a93306d549a36bb7
Berthy Feng <berthyf96@gmail.com>
2022-05-13 15:58:34 -0700

    first working version of real-cube inference demo

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 import threading
+import pickle
 import time
 
 import numpy as np
@@ -412,7 +413,6 @@ def plot_3d_cube(cube, weights, wmin=None, wmax=None, cmap=cm.viridis,
     for z in range(cube.nz):
         voxels[:, :, z*pad_height] = 1
         layer_weights = cube.layer_weights(weights, z)
-        #layer_weights = np.flip(layer_weights, axis=1)
         layer_weights = rotate(layer_weights, 90, reshape=True)
         layer_weights = np.flip(layer_weights, axis=0)
         layer_colors = cmap(norm(layer_weights))
@@ -825,3 +825,64 @@ def get_solver(cube, proj_mat):
         laplacian=L, P=P_free, G=G_free)
     
     return s
+
+
+def gather_modal_data_across_videos(info_dict_fns):
+    '''Returns all observed (normalized, real) modes and frequencies, as well as other FFT data.
+    Input:
+        info_dict_fns -- list of filenames containing pickle dictionaries.
+    Outputs:
+        data_dict (dict) -- dictionary containing all sampled 3D modes, 
+            image-space modes, etc.
+    '''
+    print('Gathering modal observation data from:')
+    for fn in info_dict_fns:
+        print('  * %s' % fn)
+
+    obs_modes_dx, obs_modes_dy, obs_freqs = [], [], []
+    obs_modes_sampled = []
+    obs_modes_powers = []
+    fft_bin_widths = []
+    for info_dict_fn in info_dict_fns:
+        with open(info_dict_fn, 'rb') as fp:
+            info_dict = pickle.load(fp)
+
+        modes_dx = info_dict['modes_dx'].real
+        modes_dy = info_dict['modes_dy'].real
+        freqs = info_dict['freqs']
+        fft_res = info_dict['fft_res']
+        powers = info_dict['powers']
+        U_observed = np.real(info_dict['U_observed'])
+
+        # Take the real part of and normalize each mode.
+        for i, (dx, dy) in enumerate(zip(modes_dx, modes_dy)):
+            norm = np.linalg.norm(np.concatenate((dx.real, dy.real)))
+            modes_dx[i] = np.real(dx) / norm
+            modes_dy[i] = np.real(dy) / norm
+        for i in range(U_observed.shape[-1]):
+            U_observed[:, i] = U_observed[:, i] / np.linalg.norm(U_observed[:, i])
+
+        obs_modes_sampled.append(U_observed)
+        obs_modes_dx.append(modes_dx)
+        obs_modes_dy.append(modes_dy)
+        obs_freqs.append(freqs)
+        obs_modes_powers.append(powers)
+        fft_bin_widths.append(fft_res)
+
+    # Process image-space modes: keep real part only and flip images vertically.
+    for vid_i in range(len(obs_modes_dx)):
+        for mode_i in range(len(obs_modes_dx[vid_i])):
+            dx = obs_modes_dx[vid_i][mode_i].real
+            dy = obs_modes_dy[vid_i][mode_i].real
+            obs_modes_dx[vid_i][mode_i] = cv2.flip(dx, 0)
+            obs_modes_dy[vid_i][mode_i] = cv2.flip(dy, 0)
+
+    data_dict = {
+        'obs_3d_modes': obs_modes_sampled,
+        'obs_modes_dx': obs_modes_dx,
+        'obs_modes_dy': obs_modes_dy,
+        'obs_freqs': obs_freqs,
+        'obs_modes_powers': obs_modes_powers,
+        'fft_bin_widths': fft_bin_widths
+    }
+    return data_dict
